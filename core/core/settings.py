f5951b9ef6bbd28c4a9ce2f2e0ab7f1876a33c82
amirsalar <hosseiniseyedamirsalar@gmail.com>
2024-10-29 17:54:11 +0330

    create two apps names home and accounts

--- core/core/settings.py
+++ core/core/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Apps
+    'home.apps.HomeConfig',
+    'accounts.apps.AccountsConfig',
+    # Packages
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
