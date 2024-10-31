71263a6335fa721355d578b838d5fe58846f46a9
amirsalar <hosseiniseyedamirsalar@gmail.com>
2024-10-31 19:47:46 +0330

    update view was created

--- core/accounts/serializers.py
+++ core/accounts/serializers.py
@@ -59,3 +59,9 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        if password:
+            instance.set_password(password)
+        return super().update(instance, validated_data)
--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,7 +1,8 @@
 from rest_framework import status
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from .models import User
 from .serializers import UserSerializer
 
 
@@ -14,4 +15,16 @@ class UserRegisterView(APIView):
         if ser_data.is_valid():
             ser_data.create(ser_data.validated_data)
             return Response(ser_data.data, status=status.HTTP_201_CREATED)
+        return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserUpdateView(APIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def put(self, request, *args, **kwargs):
+        user = User.objects.get(id=request.user.id)
+        ser_data = self.serializer_class(instance=user, data=request.data, partial=True)
+        if ser_data.is_valid():
+            ser_data.save()
+            return Response(ser_data.data, status=status.HTTP_200_OK)
         return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
