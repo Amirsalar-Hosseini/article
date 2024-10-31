bbd827f5897078169dbde5b7710a46c48906c9fd
amirsalar <hosseiniseyedamirsalar@gmail.com>
2024-10-31 19:46:57 +0330

    create refresh and access token and override access token for login view

---
+++ core/accounts/tokens.py
@@ -0,0 +1,21 @@
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['first_name'] = user.first_name
+        token['last_name'] = user.last_name
+        token['phone_number'] = user.phone_number
+        token['email'] = user.email
+        token['age'] = user.age
+        token['is_superuser'] = user.is_superuser
+        token['is_verify'] = user.is_verify
+
+        return token
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import UserRegisterView
+from .views import UserRegisterView, UserUpdateView
+from .tokens import CustomTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 app_name = 'accounts'
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='user_register'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', CustomTokenObtainPairView.as_view(), name='user_login'),
+    path('update/', UserUpdateView.as_view(), name='user_update'),
 ]
