312d1e4768e9cd381a440899c9b987d601d55ee3
amirsalar <hosseiniseyedamirsalar@gmail.com>
2024-11-01 22:11:16 +0330

    article, tag, review and like models are define

--- core/article/admin.py
+++ core/article/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Article, Tag, Review, Like
 
-# Register your models here.
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'created_at', 'updated_at', 'is_confirmed']
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name',]
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['article', 'author', 'rating', 'created_at', 'is_reply']
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ['user', 'article']
--- core/article/models.py
+++ core/article/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    poster = models.ImageField(upload_to="articles/%Y/%m/%d")
+    duration = models.IntegerField(help_text='duration in minutes for reading')
+    tags = models.ManyToManyField('Tag', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_confirmed = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.title
+
+    def likes_count(self):
+        return self.article_like.count()
+
+    def user_can_like(self, user):
+        user_like = user.user_likes.filter(article=self)
+        if user_like.exists():
+            return True
+        return False
+
+
+class Tag(models.Model):
+    """
+    for tags category
+    """
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    """
+    define user comments
+    """
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='reviews')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(choices=[(i, str(i)) for i in range(1, 6)])
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    reply = models.ForeignKey('self', on_delete=models.CASCADE, related_name='replies')
+    is_reply = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Review by {self.author.first_name} {self.author.last_name} for {self.article.title}'
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_like')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='article_like')
+
+
+    def __str__(self):
+        return f'{self.user} liked {self.article}'
