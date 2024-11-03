0b27bcb5fb94da07813dd23d05d5859b848977b4
amirsalar <hosseiniseyedamirsalar@gmail.com>
2024-11-04 00:03:44 +0330

    throttle classes was added(type default)

--- core/core/settings.py
+++ core/core/settings.py
@@ -159,6 +159,14 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '10/minute',
+        'user': '20/minute',
+    }
 }
 
 from datetime import timedelta
