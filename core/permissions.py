22c1db1656f44b85c496a4511a9996af5dd06bf9
amirsalar <hosseiniseyedamirsalar@gmail.com>
2024-11-03 23:34:32 +0330

    create a custom permission name IsOwner

--- core/permissions.py
+++ core/permissions.py
@@ -4,3 +4,12 @@ from rest_framework.permissions import BasePermission
 class IsVerify(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.is_verify
+
+class IsOwner(BasePermission):
+    def has_permission(self, request, view):
+        return True
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return True
+        return obj.author == request.user
